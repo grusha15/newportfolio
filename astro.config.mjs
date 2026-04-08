b5b4cdd852bca262218d89882cfef91fd118cd6f
grusha15 <grushags9@gmail.com>
2026-04-08 16:24:35 +0530

    Switch to static output, remove Cloudflare SSR adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  adapter: cloudflare(),
+  output: 'static',
 });
